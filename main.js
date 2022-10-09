56725283988aad40357a35654fbca7bf75cff861
Theresa Chavez <35062993+yangck1@users.noreply.github.com>
2022-10-09 09:25:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ console.clear();
 
 http.__okhttp__.setTimeout(10000);
 
-let url = [
-    'https://ghproxy.com/https://raw.githubusercontent.com/yangck1/zhu123/main/UI',
-    'https://gh.api.99988866.xyz/https://raw.githubusercontent.com/yangck1/zhu123/main/UI',
-    'https://raw.githubusercontent.com/yangck1/zhu123/main/UI',
-
-];
-
 // let url = [
-//     'https://ghproxy.com/https://raw.githubusercontent.com/yangck1/zhu123/main/UI_kaifa',
-//     'https://gh.api.99988866.xyz/https://raw.githubusercontent.com/yangck1/zhu123/main/UI_kaifa',
-//     'https://raw.githubusercontent.com/yangck1/zhu123/main/UI_kaifa',
+//     'https://ghproxy.com/https://raw.githubusercontent.com/yangck1/zhu123/main/UI',
+//     'https://gh.api.99988866.xyz/https://raw.githubusercontent.com/yangck1/zhu123/main/UI',
+//     'https://raw.githubusercontent.com/yangck1/zhu123/main/UI',
 
 // ];
+
+let url = [
+    'https://ghproxy.com/https://raw.githubusercontent.com/yangck1/zhu123/main/UI_kaifa',
+    'https://gh.api.99988866.xyz/https://raw.githubusercontent.com/yangck1/zhu123/main/UI_kaifa',
+    'https://raw.githubusercontent.com/yangck1/zhu123/main/UI_kaifa',
+
+];
 
 for (var i = 0; i < url.length; i++) {
     try {
