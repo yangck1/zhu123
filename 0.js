4e30b37b1017f44d9aae5f98f632552a386da285
Theresa Chavez <35062993+yangck1@users.noreply.github.com>
2022-12-30 10:13:46 +0800

    Update 0.js
    111

--- 0.js
+++ 0.js
@@ -16,7 +16,7 @@ var meiri = TTXS_PRO_CONFIG.get("meiri", true);
 var meizhou = TTXS_PRO_CONFIG.get("meizhou", 0);
 var zhuanxiang = TTXS_PRO_CONFIG.get("zhuanxiang", 0);
 var tiaozhan = TTXS_PRO_CONFIG.get("tiaozhan", true);
-var sanren = TTXS_PRO_CONFIG.get("sanren", true);
+//var sanren = TTXS_PRO_CONFIG.get("sanren", true);
 var ocr_choice = TTXS_PRO_CONFIG.get("ocr_choice", 0);
 var ocr_maxtime = TTXS_PRO_CONFIG.get("ocr_maxtime", "5000");
 var duizhan_mode = TTXS_PRO_CONFIG.get("duizhan_mode", 0);
@@ -2285,7 +2285,7 @@ function login(username, pwd) {
 
 function refind_jifen() {
   className("android.webkit.WebView").scrollable().findOne().scrollForward();
-  var a = className("android.widget.ListView").rowCount(15).findOne();
+  var a = className("android.widget.ListView").rowCount(14).findOne();
   21 == a.depth() ? (jifen_flag = "old", fInfo("检测为旧版界面")) : 23 == a.depth() && (jifen_flag = "new", fInfo("检测为新版界面"));
   return a
 }
@@ -2589,8 +2589,8 @@ function main(userinfo){
 /*******************主程序部分*******************/
 /********定义全局变量*********/
 var jifen_list, meizhou_dao, zhuanxiang_dao, dingyue_dao, storage_user, name, jinri, zongfen;
-var jifen_map = {"评论":11,"视频":2,"文章":1,"每日":4,"每周":14,"专项":5,"挑战":6,"四人":7,"三人":8,"双人":9,
-                "订阅":10,"本地":12},
+var jifen_map = {"评论":10,"视频":2,"文章":1,"每日":4,"每周":13,"专项":5,"挑战":6,"四人":7,"三人":8,"双人":8,
+                "订阅":9,"本地":11},
     jifen_flag = "old";
 // 分割账号
 var noverify_thread = noverify();
